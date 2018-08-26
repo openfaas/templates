52c88176efd0064e029d7a462de73c2f7f7f14fb
Andrew Seymour <commit@andrewseymour.co.uk>
2018-08-26 17:02:32 +0100

    Added php7 template (as default base version)
    Signed-off-by: Andrew Seymour <commit@andrewseymour.co.uk>

---
+++ template/php/function/src/Handler.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+/**
+ * Class Handler
+ * @package App
+ */
+class Handler
+{
+    /**
+     * @param string $data
+     * @return string
+     */
+    public function handle(string $data): string {
+        return $data;
+    }
+}
