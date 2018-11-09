1b6288b8b9560b8edc139320b7c6f39519db8008
Alex Ellis (VMware) <alexellis2@gmail.com>
2018-11-09 18:12:46 +0000

    Add Copyright notice where missing
    Even though I have sole personal copyright on some of the files
    being changed here I wanted to acknowledge the source of the files
    so that if they are copied this is kept as part of the history.
    
    Signed-off-by: Alex Ellis (VMware) <alexellis2@gmail.com>

--- template/go-armhf/main.go
+++ template/go-armhf/main.go
@@ -1,12 +1,15 @@
+// Copyright (c) Alex Ellis 2017. All rights reserved.
+// Copyright (c) OpenFaaS Author(s) 2018. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
 package main
 
 import (
 	"fmt"
+	"handler/function"
 	"io/ioutil"
 	"log"
 	"os"
-
-	"handler/function"
 )
 
 func main() {
