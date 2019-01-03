9d439933aa5bc4f922a010ae7f03da36df6d1938
Rishabh Gupta <r.g.gupta@outlook.com>
2019-01-03 17:35:04 +0530

    Adds csharp armhf template. Signed-off-by: Rishabh Gupta <r.g.gupta@outlook.com>

---
+++ template/csharp-armhf/function/FunctionHandler.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Text;
+
+namespace Function
+{
+    public class FunctionHandler
+    {
+        public string Handle(string input) {
+            return string.Format("Hi there - your input was: {0}\n", input);
+        }
+    }
+}
