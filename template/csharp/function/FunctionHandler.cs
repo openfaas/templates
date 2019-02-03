a89eb0318398c908e6ca31aa0c9fbde5bbf1c630
Matthias <gitlab@matthias-simonis.de>
2019-02-03 23:54:44 +0100

    Changed csharp-templates to string interpolation
    String interpolation is more readable and the compiler can decide wether
    to use concatination or string.format.
    
    Signed-off-by: Matthias Simonis <github@matthias-simonis.de>

--- template/csharp/function/FunctionHandler.cs
+++ template/csharp/function/FunctionHandler.cs
@@ -6,7 +6,7 @@ namespace Function
     public class FunctionHandler
     {
         public string Handle(string input) {
-            return string.Format("Hi there - your input was: {0}\n", input);
+            return $"Hi there - your input was: {input}\n";
         }
     }
 }
