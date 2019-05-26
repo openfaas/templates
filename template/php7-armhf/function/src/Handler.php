67ab0634fd6720b520671e72f4bdf9ca68821418
Richard Gee <richard@technologee.co.uk>
2019-05-26 14:45:29 +0100

    Adds PHP7 armhf template
    Signed-off-by: Richard Gee <richard@technologee.co.uk>

---
+++ template/php7-armhf/function/src/Handler.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+/**
+ * Class Handler
+ * @package App
+ */
+class Handler
+{
+    /**
+     * @param $data
+     * @return
+     */
+    public function handle($data) {
+        return $data;
+    }
+}
