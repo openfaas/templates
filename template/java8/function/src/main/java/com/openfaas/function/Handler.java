e56aff343dc00f6d1218c18f2837eab920b3fc43
Julian Pardo <jd.pardo@uniandes.edu.co>
2019-07-18 15:39:07 -0500

    Added java documentation

--- template/java8/function/src/main/java/com/openfaas/function/Handler.java
+++ template/java8/function/src/main/java/com/openfaas/function/Handler.java
@@ -5,6 +5,13 @@ import com.openfaas.model.IResponse;
 import com.openfaas.model.IRequest;
 import com.openfaas.model.Response;
 
+/**
+ * See https://github.com/openfaas/templates/blob/master/template/java8/model/src/main/java/com/openfaas/model/IRequest.java
+ * for the request interface, and 
+ * https://github.com/openfaas/templates/blob/master/template/java8/model/src/main/java/com/openfaas/model/IResponse.java
+ * for the response interface
+ */
+
 public class Handler implements com.openfaas.model.IHandler {
 
     public IResponse Handle(IRequest req) {
