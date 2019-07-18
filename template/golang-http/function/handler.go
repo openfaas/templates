5ec90aa5bb118b1ec919800e8226d86680ae4f0c
Julian Pardo <jd.pardo@uniandes.edu.co>
2019-07-18 15:11:20 -0500

    Added go documentation

--- template/golang-http/function/handler.go
+++ template/golang-http/function/handler.go
@@ -3,17 +3,28 @@ package function
 import (
 	"fmt"
 	"net/http"
-
+	"net/textproto"
 	"github.com/openfaas-incubator/go-function-sdk"
 )
+
+/**
+ * Request contains the following fields:
+ *	Header:      r.Header,
+ *	Method:      r.Method,
+ *	QueryString: r.URL.RawQuery,
+ * This template is based on openfaas-incubator template. 
+ *	For further information look https://github.com/openfaas-incubator/golang-http-template 
+*/
 
 // Handle a function invocation
 func Handle(req handler.Request) (handler.Response, error) {
 	var err error
 
 	message := fmt.Sprintf("Hello world, input was: %s", string(req.Body))
-
+	header := make(map[string][]string)
+	textproto.MIMEHeader(header).Add("Authorization", "Bearer ...")
 	return handler.Response{
+		Header:		header,
 		Body:       []byte(message),
 		StatusCode: http.StatusOK,
 	}, err
