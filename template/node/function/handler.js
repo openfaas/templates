f94e3955b05701cbbd6e8c59b68562ddafe9c6e7
Patricio Diaz <padiazg@gmail.com>
2019-09-17 17:31:41 -0400

    node template to support promise/async/await
    Signed-off-by: Patricio Diaz <padiazg@gmail.com>

--- template/node/function/handler.js
+++ template/node/function/handler.js
@@ -1,5 +1,33 @@
 "use strict"
 
-module.exports = (context, callback) => {
-    callback(undefined, {status: "done"});
+// A simple delay function
+const delayCallback = cb => {
+    const min = 0; // 0 sec
+    const max = 2; // 2 sec
+    const delay = Math.round((Math.random() * (max - min) + min));
+    setTimeout(() => cb(delay),  delay * 1000);
 }
+
+// callback version
+module.exports = (context, callback) => {
+    delayCallback(delay => callback(undefined, {status: "done", delay}))    
+}
+
+// Uncomment the following line to use it with the promise or async/await versions
+//const delayPromise = () => new Promise(resolve => delayCallback(delay => resolve(delay)) )
+
+/*
+module.exports = context => new Promise((resolve, reject) => {
+    delayPromise()
+    .then(delay => {
+        resolve({status: "promise", delay});
+    })
+})
+*/
+
+/*
+module.exports = async context => {
+    const delay = await delayPromise();
+    return {status: "async/await", delay}
+}
+*/
