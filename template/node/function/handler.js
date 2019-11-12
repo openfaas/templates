047269fa891eff8445b152207e7937c547fd5854
Patricio Diaz <padiazg@gmail.com>
2019-11-11 22:53:12 -0300

    Cleaned code for handler
    Signed-off-by: Patricio Diaz <padiazg@gmail.com>

--- template/node/function/handler.js
+++ template/node/function/handler.js
@@ -1,33 +1,6 @@
 "use strict"
 
-// A simple delay function
-const delayCallback = cb => {
-    const min = 0; // 0 sec
-    const max = 2; // 2 sec
-    const delay = Math.round((Math.random() * (max - min) + min));
-    setTimeout(() => cb(delay),  delay * 1000);
-}
-
 // callback version
-module.exports = (context, callback) => {
-    delayCallback(delay => callback(undefined, {status: "done", delay}))    
+module.exports = async (context, callback) => {
+    return {status: "done"}
 }
-
-// Uncomment the following line to use it with the promise or async/await versions
-//const delayPromise = () => new Promise(resolve => delayCallback(delay => resolve(delay)) )
-
-/*
-module.exports = context => new Promise((resolve, reject) => {
-    delayPromise()
-    .then(delay => {
-        resolve({status: "promise", delay});
-    })
-})
-*/
-
-/*
-module.exports = async context => {
-    const delay = await delayPromise();
-    return {status: "async/await", delay}
-}
-*/
