b530d06164b87b835ce3249aa5b66f1e4d6aa74b
Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>
2020-02-08 12:39:46 +0000

    Apply linting for node12
    Signed-off-by: Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>

--- template/node12/function/handler.js
+++ template/node12/function/handler.js
@@ -1,12 +1,11 @@
-"use strict"
+'use strict'
 
 module.exports = async (event, context) => {
-    let err;
-    const result =             {
-        status: "Received input: " + JSON.stringify(event.body)
-    };
+  const result = {
+    'status': 'Received input: ' + JSON.stringify(event.body)
+  }
 
-    return context
-        .status(200)
-        .succeed(result);
+  return context
+    .status(200)
+    .succeed(result)
 }
