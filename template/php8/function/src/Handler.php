ac9b4b10737e1a9583aa063d8803d740849f52cc
meacu1pa <25086505+meacu1pa@users.noreply.github.com>
2022-05-25 21:00:53 +0200

    Init PHP 8.1 template
    Bump composer version to :latest
    
    Update list of enabled php modules
    
    Remove Phalcon example and update documentation
    Phalcon currently has no PHP 8 support, that's why it's been removed.
    
    Bump classic-watchdog version to 0.2.1
    
    Signed-off-by: Dominic Schuld <meaculpa@posteo.de>

---
+++ template/php8/function/src/Handler.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+/**
+ * Class Handler
+ * @package App
+ */
+class Handler
+{
+    /**
+     * @param string $data
+     * @return string
+     */
+    public function handle(string $data): string
+    {
+        return $data;
+    }
+}
